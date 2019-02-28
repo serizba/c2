23c943fb8d1e7d393b0b85c4cbaac89457fe55e8
Sergio Izquierdo <serizba@gmail.com>
2019-02-28 21:02:51 +0100

    algo va

--- c2.py
+++ c2.py
@@ -1,31 +1,99 @@
 import numpy as np
-import pandas as pd
+from tqdm import tqdm
+def puntuation(tags_1,tags_2):
+	i = np.intersect1d(tags_1,tags_2)
+	r1 = np.setdiff1d(tags_1,i)
+	r2 = np.setdiff1d(tags_2,i)
+	return min(i.size,r1.size,r2.size)
 
-with open("./b_lovely_landscapes.txt") as f:
+dictags = {}
+
+file = './input/b_lovely_landscapes.txt'
+#file = "./input/c_memorable_moments.txt"
+with open(file) as f:
     N = f.readline()
     imgs = []
     res = []
+    v = None
     for i, line in enumerate(f):
     	t, _, *tags = line.split()
-    	imgs.append((i, tags, t))
+    	if t == 'V':
+    		if v is None:
+    			v = (i, tags, -2)
+    			continue
+    		else:
+    			tags = np.union1d(v[1], tags)
+    			imgs.append((v[0], tags, i))
+    			v = None
+    	else:
+    		imgs.append((i, tags, -1))
+    	for t in tags:
+    		if t in dictags:
+    			dictags[t].append(i)
+    		else:
+    			dictags[t] = [i]
+    	
+
+res.append(0)
+
+while len(res) < len(imgs):
+	#print(len(res))
+	for tag in imgs[res[-1]][1]:
+		posibles = [imgs[i] for i in dictags[tag] if not i in res]
+		if len(posibles) > 0:
+			break
+
+	if len(posibles) == 0:
+		for i in range(len(res)):
+			if i not in res:
+				posibles = [imgs[i]]
+				break
+
+	#print(len(posibles))
+	res.append(list(sorted(list(map(lambda x: (puntuation(imgs[res[-1]][1], x[1]), x[0]), posibles)), key=lambda x: x[0]))[0][1])
 
 
-actual = imgs[0]
-res[0] = 0
-while len(imgs) > 0:
-	posibles = [img for img in imgs if imgs[0] != actual[0]]  
-	list(lambda x: (puntuacion()))
 
+print(dictags['tgrww'])
+
+# res.append(0)
+# while len(imgs) > 0:
+# 	posibles = [img for img in imgs if not imgs[0] in res]
+# 	#print("caca")  
+# 	best = list(sorted(list(map(lambda x: (puntuation(imgs[res[-1]][1], x[1]), x[0]), posibles)), key=lambda x: x[0]))[0][1]
+# 	print(best)
+# 	res.append(best)
     #     vlist.append(tuple(map(lambda x: int(x),line.split()+[i])))
     #     i=i+1
     # fl = list(map(lambda x: int(x),f.readline().split()))
 
+# P = np.ones((10000, 10000), dtype=int) * -1
+# P[:,0] = 0
+# S = np.zeros((10000, 10000), dtype=int)
+# S[:,0] = np.arange(10000)
+
+# for i in tqdm(range(1,10000)):
+# 	for j in range(1,i+1):
+# 		anyadir = P[i-1,j-10] + puntuation(imgs[S[i-1, j-1]][1], imgs[i][1])
+# 		noany = P[i-1, j]
+# 		P[i,j] = max(anyadir, noany)
+# 		if anyadir > noany:
+# 			S[i,j] = i
+# 		else:
+# 			S[i,j] = S[i-1, j]
+
+# print(i)
+
+# Reconstruir
 
 
-with open("./result.txt","w+") as fw:
+with open("./output/result.txt","w+") as fw:
 	fw.write(str(len(res))+"\n")
 	for r in res:
-		fw.write(str(r)+"\n")
+		if (imgs[r][2] >= 0):
+			fw.write(str(r)+" "+str(imgs[r][2])+"\n")
+		else:
+			fw.write(str(r)+"\n")
     # for i in range(0,nc):
     #     strres = ""
     #     for j in range(0,len(result[i])):
--- puntuacion.py
+++ puntuacion.py
@@ -1,6 +1,6 @@
 import numpy as np 
 
-def puntuation(tags_1,tags_2)
+def puntuation(tags_1,tags_2):
 	i = np.intersect1d(tags_1,tags_2)
 	r1 = np.setdiff1d(tags_1,i)
 	r2 = np.setdiff1d(tags_2,i)
