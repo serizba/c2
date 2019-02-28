ac68dd94606f0fa81bf4ba101a87c444b2738b8c
juliagviu <juliagviu97@gmail.com>
2019-02-28 19:15:37 +0100

    puntuacion

---
+++ puntuacion.py
@@ -0,0 +1,7 @@
+import numpy as np 
+
+def puntuation(tags_1,tags_2)
+	i = np.intersect1d(tags_1,tags_2)
+	r1 = np.setdiff1d(tags_1,i)
+	r2 = np.setdiff1d(tags_2,i)
+	return min(i.size,r1.size,r2.size)
