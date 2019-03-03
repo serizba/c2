90c8508d6e2fae9ea0ecc4c60c1cf9fe4a719da6
Sergio Izquierdo <serizba@gmail.com>
2019-03-03 20:43:26 +0100

    m

--- c2.py
+++ c2.py
@@ -1,88 +1,39 @@
 import numpy as np
 from tqdm import tqdm
+
 def puntuation(tags_1,tags_2):
 	i = np.intersect1d(tags_1,tags_2)
 	r1 = np.setdiff1d(tags_1,i)
 	r2 = np.setdiff1d(tags_2,i)
 	return min(i.size,r1.size,r2.size)
 
-dictags = {}
 
-# #file = './input/b_lovely_landscapes.txt'
-# file = "./input/c_memorable_moments.txt"
-# with open(file) as f:
-#     N = f.readline()
-#     imgs = []
-#     res = []
-#     v = None
-#     for i, line in enumerate(f):
-#     	t, _, *tags = line.split()
-#     	if t == 'V':
-#     		if v is None:
-#     			v = (i, tags, -2)
-#     			continue
-#     		else:
-#     			tags = np.union1d(v[1], tags)
-#     			imgs.append((i, tags, v[0]))
-#     			v = None
-#     	else:
-#     		imgs.append((i, tags, -1))
-#     	for t in tags:
-#     		if t in dictags:
-#     			dictags[t].append(i)
-#     		else:
-#     			dictags[t] = [i]
-    
-#file = './input/b_lovely_landscapes.txt'
-#file = "./input/c_memorable_moments.txt"
-#file = './input/d_pet_pictures.txt'
-file = './input/newdaa'
-# with open(file) as f:
-#     N = f.readline()
-#     imgs = []
-#     res = []
-#     v = None
-#     for i, line in enumerate(f):
-#     	t, _, *tags = line.split()
-#     	if t == 'V':
-#     		if v is None:
-#     			v = (i, tags, -2)
-#     			imgs.append([None])
-#     			continue
-#     		else:
-#     			tags = np.union1d(v[1], tags)
-#     			imgs.append((i, tags, v[0]))
-#     			v = None
-#     	else:
-#     		imgs.append((i, tags, -1))
-#     	for t in tags:
-#     		if t in dictags:
-#     			dictags[t].append(i)
-#     		else:
-#     			dictags[t] = [i]
 
+file = './input/newdae'
+# Dictionary storing images {id_image : (id_image, tags_image, vertical_pair)}
+imgs = {}
+# Dictionary storing tags {tag_id : [id_image]}
+tags = defaultdict(list)
+
+# Read and parse input file
 with open(file) as f:
-    N = int(f.readline())
-    imgs = {}
-    res = []
+	# Ignore first line
+    f.readline()
     v = None
     for i, line in enumerate(f):
-    	t, _, *tags = line.split()
+    	t, _, *tags_img = line.split()
     	if t == 'V':
     		if v is None:
     			v = (i+N, tags, -2)
     			continue
     		else:
     			tags = np.union1d(v[1], tags)
-    			imgs[i+N] = (i+N, tags, v[0])
+    			imgs[i] = (i, tags, v[0])
     			v = None
     	else:
-    		imgs[i+N] = (i+N, tags, -1)
+    		imgs[i] = (i, tags, -1)
     	for t in tags:
-    		if t in dictags:
-    			dictags[t].append(i+N)
-    		else:
-    			dictags[t] = [i+N]
+    			dictags[t].append(i)
 
 res = {}
 last = None
@@ -100,8 +51,8 @@ while len(res) < len(imgs):
 		# for i in dictags[tag]:
 		# 	print(i, len(imgs))
 		# print('caca')
-		if len(dictags[tag]) > 500:
-			posibles = [imgs[i] for i in np.random.choice(dictags[tag], 500, False) if not i in res]
+		if len(dictags[tag]) > 1000:
+			posibles = [imgs[i] for i in np.random.choice(dictags[tag], 1000, False) if not i in res]
 		else:
 			posibles = [imgs[i] for i in dictags[tag] if not i in res]
 		#posibles = np.random.choice(np.array(posibles), 200, False)
@@ -129,40 +80,7 @@ while len(res) < len(imgs):
 	#last = last[1]
 	res[last] = 1
 
-
-
-# res.append(0)
-# while len(imgs) > 0:
-# 	posibles = [img for img in imgs if not imgs[0] in res]
-# 	#print("caca")  
-# 	best = list(sorted(list(map(lambda x: (puntuation(imgs[res[-1]][1], x[1]), x[0]), posibles)), key=lambda x: x[0]))[0][1]
-# 	print(best)
-# 	res.append(best)
-    #     vlist.append(tuple(map(lambda x: int(x),line.split()+[i])))
-    #     i=i+1
-    # fl = list(map(lambda x: int(x),f.readline().split()))
-
-# P = np.ones((10000, 10000), dtype=int) * -1
-# P[:,0] = 0
-# S = np.zeros((10000, 10000), dtype=int)
-# S[:,0] = np.arange(10000)
-
-# for i in tqdm(range(1,10000)):
-# 	for j in range(1,i+1):
-# 		anyadir = P[i-1,j-10] + puntuation(imgs[S[i-1, j-1]][1], imgs[i][1])
-# 		noany = P[i-1, j]
-# 		P[i,j] = max(anyadir, noany)
-# 		if anyadir > noany:
-# 			S[i,j] = i
-# 		else:
-# 			S[i,j] = S[i-1, j]
-
-# print(i)
-
-# Reconstruir
-
-
-with open("./output/resulteaa.txt","w+") as fw:
+with open("./output/result-ae.txt","w+") as fw:
 	fw.write(str(len(res))+"\n")
 	for r in res.keys():
 		if (imgs[r][2] >= 0):
